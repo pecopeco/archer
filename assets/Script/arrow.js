4d1f99d52ecd221940929d3bbff2bf5e0f35ac49
pecopeco <yangpeco@qq.com>
2019-04-08 18:39:53 +0800

    优化射击角度

--- assets/Script/arrow.js
+++ assets/Script/arrow.js
@@ -3,8 +3,6 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // 设置弓箭射出加速度
-        accel: 0,
         arch: {
             default: null,
             type: cc.Node
@@ -14,15 +12,18 @@ cc.Class({
     // LIFE-CYCLE CALLBACKS:
 
     onLoad: function () {
+
         // 弓箭初始位置
         this.startX = this.node.x
         this.startY = this.node.y
+
         // 弓箭射出开关
         this.fly = false
 
-        // 弓箭拉出垂直距离
+        // 弓箭拉出距离
         this.yLength = 0
         this.xLength = 0
+
         // 弓箭拉出角度
         this.arrowRotation = 0
         
@@ -33,12 +34,12 @@ cc.Class({
         this.node.on('touchmove', (e) => {
             this.yLength = e.getLocation().y - e.getStartLocation().y
             this.xLength = e.getLocation().x - e.getStartLocation().x
-            // 防止拉出屏幕或点击
+            // 防止拉出屏幕或点击就发射
             if (this.yLength < -180 || this.yLength > -10) return
-            // 设置角度
-            this.arrowRotation = Math.atan(this.xLength / -200)*180/Math.PI
+            // 设置拉动角度
+            this.arrowRotation = Math.atan(this.xLength / (this.yLength - 50)) * 180 / Math.PI
             this.node.parent.rotation = this.arrowRotation
-            // 设置距离
+            // 设置拉动距离
             this.node.y = this.startY + (this.yLength * 3) - ((Math.abs(this.arrowRotation) / 45) * 200)
         })
         // 拉动后放手
@@ -60,8 +61,8 @@ cc.Class({
     update (dt) {
         // 弓箭开始飞行
         if (this.fly) {
-            this.node.y += this.accel * dt
-            this.node.x += this.arrowRotation * 30 * dt
+            this.node.y += 9000 * dt
+            this.node.x += this.arrowRotation * dt
         }
         // 弓箭飞出屏幕重置状态
         if (this.node.y > 5000) {
