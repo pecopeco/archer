2da602273809a7a920e23d03e3d8d1964897a3bb
pecopeco <yangpeco@qq.com>
2019-04-23 18:28:18 +0800

    添加排行榜、用户昵称系统

---
+++ assets/Script/input.js
@@ -0,0 +1,43 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        user: {
+            default: null,
+            type: cc.Label
+        }
+    },
+
+    onEditDidBegan: function(editbox, customEventData) {
+        //这里 editbox 是一个 cc.EditBox 对象
+        //这里的 customEventData 参数就等于你之前设置的 "foobar"
+    },
+    //假设这个回调是给 editingDidEnded 事件的
+    onEditDidEnded: function(editbox, customEventData) {
+        //这里 editbox 是一个 cc.EditBox 对象
+        //这里的 customEventData 参数就等于你之前设置的 "foobar"
+    },
+    //假设这个回调是给 textChanged 事件的
+    onTextChanged: function(text, editbox, customEventData) {
+        //这里的 text 表示 修改完后的 EditBox 的文本内容
+        //这里 editbox 是一个 cc.EditBox 对象
+        //这里的 customEventData 参数就等于你之前设置的 "foobar"
+        cc.sys.localStorage.setItem("name", text)
+        this.user.string = text
+    },
+    //假设这个回调是给 editingReturn 事件的
+    onEditingReturn: function(editbox,  customEventData) {
+        //这里 editbox 是一个 cc.EditBox 对象
+        //这里的 customEventData 参数就等于你之前设置的 "foobar"
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+    },
+
+    // update (dt) {},
+});
--- assets/Script/startScene.js
+++ assets/Script/startScene.js
@@ -10,24 +10,73 @@ cc.Class({
             default: null,
             type: cc.Label
         },
+        user: {
+            default: null,
+            type: cc.Label
+        },
 		startBtn: {
             default: null,
             type: cc.Label
         }
     },
+
+    postScore: function () {
+        // 声明一个 Todo 类型
+        let AV
+        if (window.SAVE && window.SAVE.AV) {
+            AV = window.SAVE.AV
+        } else {
+            AV = require('leancloud-storage')
+            AV.init("2kq3azst6ToDirUHcGqAdnNp-gzGzoHsz", "lUAeHSFwalgva7GU4vpRT6Lo")
+            window.SAVE = {
+                AV: AV
+            }
+        }
+        if (window.ID && window.ID.ID) {
+            var todo = AV.Object.createWithoutData('Todo', window.ID.ID);
+        } else {
+            // 新建一个 Todo 对象
+            var Todo = AV.Object.extend('Todo')
+            var todo = new Todo()
+            if (cc.sys.localStorage.getItem("name")) {
+                todo.set('name', cc.sys.localStorage.getItem("name"))
+            }
+        }
+        if (cc.sys.localStorage.getItem("name")) {
+            todo.set('score', cc.sys.localStorage.getItem("score"))
+            todo.save().then(function (todo) {
+                // 成功保存之后，执行其他逻辑.
+                console.log('New object created with objectId: ' + todo.id)
+                window.ID = {
+                    ID: todo.id
+                }
+            }, function (error) {
+                // 异常处理
+                console.error('Failed to create new object, with error message: ' + error.message)
+            })
+        }
+    },
     
     // use this for initialization
     onLoad: function () {
+        // 更新本局得分
 		if (window.Global) {
             this.score.string = '本局得分: ' + window.Global.score
             this.startBtn.string = '重新开始'
 		} else {
             this.score.destroy()
         }
+        // 更新历史得分
         if (cc.sys.localStorage.getItem("score")) {
             this.highScore.string = '历史最高得分: ' + cc.sys.localStorage.getItem("score")
         } else {
             this.highScore.destroy()
         }
+        // 显示名字
+        if (cc.sys.localStorage.getItem("name")) {
+            this.user.string = cc.sys.localStorage.getItem("name")
+        }
+        // 数据存储
+        this.postScore()
     }
-});
+})
